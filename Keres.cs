c1158d8b5f8c8cbe37e1cb2aeb838d62f1198c7f
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-09-09 15:10:35 +0200

    Add project files.

---
+++ Keres.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Reflection.Emit;
+
+namespace CLI24090903
+{
+    internal class Keres
+    {
+        public string Cim { get; set; }
+        public DateTime Ido { get; set; }
+        public string Kep { get; set; }
+        public int Kod { get; set; }
+        public int? Meret { get; set; }
+        public bool Domain => !char.IsNumber(Cim[^1]);
+
+        public Keres(string beolvasottSor)
+        {
+            string[] darabok = beolvasottSor.Split('*');
+            Cim = darabok[0];
+            Ido = DateTime.ParseExact(
+                darabok[1], 
+                "dd/MMM/yyyy:HH:mm:ss", 
+                CultureInfo.InvariantCulture);
+            Kep = darabok[2];
+            string[] kodmeret = darabok[3].Split(' ');
+            Kod = int.Parse(kodmeret[0]);
+            Meret = kodmeret[1] == "-" ? null : int.Parse(kodmeret[1]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using CLI24090903;
+
+List<Keres> keresek = [];
+using StreamReader sr = new(@"..\..\..\src\NASAlog.txt");
+while (!sr.EndOfStream) keresek.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"5. feladat: keresek szama: {keresek.Count}");
+
+int? osszMeret = keresek.Sum(k => k.Meret);
+Console.WriteLine($"6. feladat: valaszok osszes merete: {osszMeret} byte");
+
+int vanDomain = keresek.Count(k => k.Domain);
+Console.WriteLine($"8. feladat: domain-es keresek: " +
+    $"{(float)vanDomain / keresek.Count * 100:0.00}%");
+
+var statisztika = keresek.GroupBy(k => k.Kod);
+Console.WriteLine("9. feladat: statisztika:");
+foreach (var grp in statisztika)
+{
+    Console.WriteLine($"\t{grp.Key}: {grp.Count()} db");
+}
